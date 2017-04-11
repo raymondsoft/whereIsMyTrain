e75b20fbb50cba3c106eba9673630711424c33bb
etudiant-09 <etudiant-09@etudiant-09.local>
2017-04-11 17:21:39 +0200

    Ajout launchScreen & icones

--- whereIsMyTrain/AppDelegate.swift
+++ whereIsMyTrain/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         let isStationMetroLoaded = UserDefaults.standard.bool(forKey: "StationsMetroLoaded")
-        if isStationMetroLoaded {
+        if !isStationMetroLoaded {
             loadData()
             UserDefaults.standard.set(true, forKey: "StationsMetroLoaded")
         }
---
+++ whereIsMyTrain/Controllers/LaunchScreenViewController.swift
@@ -0,0 +1,62 @@
+//
+//  LaunchScreenViewController.swift
+//  whereIsMyTrain
+//
+//  Created by etudiant-09 on 11/04/2017.
+//  Copyright © 2017 etudiant-09. All rights reserved.
+//
+
+import UIKit
+
+class LaunchScreenViewController: UIViewController {
+
+    var hue : CGFloat = 0
+    var saturation : CGFloat = 0
+    var brightness : CGFloat = 0
+    var alpha : CGFloat = 0
+    
+    var timer : Timer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        if (self.view.backgroundColor?.getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: &alpha))! {
+            print(hue)
+            print(brightness)
+            print(saturation)
+            print(alpha)
+        }
+//        self.timer = Timer(timeInterval: 0.1, target: self, selector: #selector(LaunchScreenViewController.lighterBackground), userInfo: nil, repeats: true)
+        self.timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: lighterBackground(with: ))
+//        self.timer.fire()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    func lighterBackground(with timer : Timer) {
+        self.saturation = self.saturation - 0.1
+        self.view.backgroundColor = UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: alpha)
+        print("yo \(self.hue)")
+        if self.saturation < 0 {
+            self.timer.invalidate()
+            self.performSegue(withIdentifier: "LaunchToTabBar", sender: self)
+        }
+    }
+    
+}
